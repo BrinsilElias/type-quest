e6e390a1af1b59736d6c34a69b564ffee6de81aa
Brinsil Elias <brinsilelias0592@gmail.com>
2025-01-09 21:45:15 +0530

    feat: create multiple typing mode and wpm calculator

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,17 +1,10 @@
-import $ from 'jquery';
-
-declare global {
-  interface Window {
-    $: typeof $;
-  }
-}
-
-window.$ = $;
-
 const $textField = $('.wordlist');
 const $inputField = $('#keyboard-input');
 
-let timer: number;
+const typingMode = $('input[name="typing-mode"]:checked').val() as
+  | 'timer'
+  | 'wordCount';
+
 let startTime: number;
 let wordList: string[];
 let currentWord: number;
@@ -20,58 +13,98 @@ const correctWords: string[] = [];
 
 currentWord = 0;
 isTimerActive = false;
-const testMode = $('input.test-mode:checked').val() as 'timer' | 'wordCount';
 
-function getWordList() {
-  const quote = Math.floor(Math.random() * (30 - 0 + 1)) + 1;
-  fetch(`https://dummyjson.com/quotes/${quote}`)
+function setText(_lang: string) {
+  wordList = [];
+  const wordCount = 25;
+  const lang = _lang.toLowerCase();
+  fetch('/random.json')
     .then((response) => response.json())
     .then((data) => {
-      $textField.text(data.quote);
-      wordList = data.quote.split(' ');
-    });
+      const randomWords = data[lang];
+      while (wordList.length < wordCount) {
+        const randInx = Math.floor(Math.random() * randomWords.length);
+        const randomWord = randomWords[randInx] || '';
+        wordList.push(randomWord);
+        $textField.append(`<span>${randomWord} </span>`);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
 function startTimer() {
-  switch (testMode) {
+  switch (typingMode) {
     case 'timer':
-      // Start a timer of specified time
-      return 0;
-    case 'wordCount': {
-      startTime = 0;
-      const timerId = window.setInterval(() => {
-        startTime++;
+      let seconds = 10;
+      const timer = window.setInterval(() => {
+        seconds--;
+        if (seconds === 0) {
+          window.clearInterval(timer);
+          isTimerActive = false;
+        }
       }, 1000);
-      return timerId;
-    }
+      break;
+    case 'wordCount':
+      startTime = Date.now();
+      break;
   }
+}
+
+function showResult() {
+  let words, minute, acc;
+  switch (typingMode) {
+    case 'timer':
+      words = correctWords.length;
+      minute = 10 / 60;
+      break;
+    case 'wordCount':
+      words = correctWords.length;
+      minute = (Date.now() - startTime) / 1000 / 60;
+      break;
+  }
+  acc = Math.floor((words / wordList.length) * 100);
+  let wpm = Math.floor(words / minute);
+  console.log(`WPM: ${wpm}`);
+  console.log(`Accuracy: ${acc}%`);
 }
 
 $inputField.on('keydown', (e) => {
   if (currentWord === 0 && !isTimerActive && /^[a-zA-Z]$/.test(e.key)) {
-    timer = startTimer();
+    startTimer();
     isTimerActive = true;
   }
   if (e.key === ' ') {
     e.preventDefault();
-
     if ($inputField.val() !== '') {
-      if (currentWord < wordList.length - 1) {
-        const inputWord = $inputField.val();
-        if (inputWord && inputWord === wordList[currentWord]) {
-          correctWords.push(inputWord);
-        }
-        currentWord++;
-      } else {
-        window.clearInterval(timer);
-        console.log(startTime);
-        console.log(correctWords);
-        $inputField.prop('disabled', true);
+      switch (typingMode) {
+        case 'timer':
+          if (!isTimerActive) {
+            $inputField.prop('disabled', true);
+            showResult();
+            return;
+          }
+          const inputWord = $inputField.val();
+          if (inputWord && inputWord === wordList[currentWord]) {
+            correctWords.push(inputWord);
+          }
+          currentWord++;
+          break;
+        case 'wordCount':
+          if (currentWord < wordList.length - 1) {
+            const inputWord = $inputField.val();
+            if (inputWord && inputWord === wordList[currentWord]) {
+              correctWords.push(inputWord);
+            }
+            currentWord++;
+          } else {
+            $inputField.prop('disabled', true);
+            showResult();
+          }
+          break;
       }
     }
-
     $inputField.val('');
   }
 });
 
-getWordList();
+setText('english');
