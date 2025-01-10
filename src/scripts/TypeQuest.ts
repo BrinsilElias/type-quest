0c55dc2d4c99fbca28a21cf1c528d5f418f94eb9
Brinsil Elias <brinsilelias0592@gmail.com>
2025-01-10 20:38:53 +0530

    chore: refactor code to use class based approach

---
+++ src/scripts/TypeQuest.ts
@@ -0,0 +1,159 @@
+class TypeQuest {
+  language: string;
+  wordCount: number;
+  timerDuration: number;
+  includeNumbers: boolean;
+  includePunctuation: boolean;
+  typingMode: 'timer' | 'wordCount';
+
+  private startTime: number;
+  private matchedWords: string[];
+  private isTimerActive: boolean;
+  private currentWordIndex: number;
+  private generatedWords: string[];
+
+  constructor(
+    language: string,
+    wordCount: number,
+    timerDuration: number,
+    includeNumbers: boolean,
+    includePunctuation: boolean,
+    typingMode: 'timer' | 'wordCount'
+  ) {
+    this.language = language;
+    this.wordCount = wordCount;
+    this.typingMode = typingMode;
+    this.timerDuration = timerDuration;
+    this.includeNumbers = includeNumbers;
+    this.includePunctuation = includePunctuation;
+
+    this.startTime = 0;
+    this.matchedWords = [];
+    this.generatedWords = [];
+    this.currentWordIndex = 0;
+    this.isTimerActive = false;
+
+    this.startTimer = this.startTimer.bind(this);
+    this.fetchWordList = this.fetchWordList.bind(this);
+    this.displayResults = this.displayResults.bind(this);
+  }
+
+  startTimer() {
+    this.isTimerActive = true;
+    switch (this.typingMode) {
+      case 'timer':
+        let remainingSeconds = this.timerDuration;
+        const timerId = window.setInterval(() => {
+          remainingSeconds--;
+          if (remainingSeconds === 0) {
+            window.clearInterval(timerId);
+            this.isTimerActive = false;
+          }
+        }, 1000);
+        break;
+      case 'wordCount':
+        this.startTime = Date.now();
+        break;
+    }
+  }
+
+  async fetchWordList($container: JQuery<HTMLElement>) {
+    try {
+      let randomWords;
+      const request = new Request('/random.json');
+      const cacheName = 'word-list-cache';
+      const cache = await caches.open(cacheName);
+      const lang = this.language.toLowerCase();
+      const cachedResponse = await cache.match(request);
+      if (cachedResponse) {
+        const cachedData = await cachedResponse.json();
+        randomWords = cachedData[lang];
+      } else {
+        const response = await fetch(request);
+        if (response.ok) {
+          await cache.put(request, response.clone());
+          const data = await response.json();
+          randomWords = data[lang];
+        }
+        throw new Error('Failed to fetch word list from network');
+      }
+      while (this.generatedWords.length < this.wordCount) {
+        const randomIndex = Math.floor(Math.random() * randomWords.length);
+        const randomWord = randomWords[randomIndex] || '';
+        this.generatedWords.push(randomWord);
+        $container.append(`<span>${randomWord} </span>`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  displayResults() {
+    let totalWordsTyped, minutesElapsed;
+    switch (this.typingMode) {
+      case 'timer':
+        totalWordsTyped = this.matchedWords.length;
+        minutesElapsed = this.timerDuration / 60;
+        break;
+      case 'wordCount':
+        totalWordsTyped = this.matchedWords.length;
+        minutesElapsed = (Date.now() - this.startTime) / 1000 / 60;
+        break;
+    }
+    const accuracy = Math.floor(
+      (totalWordsTyped / this.generatedWords.length) * 100
+    );
+    const wordsPerMinute = Math.floor(totalWordsTyped / minutesElapsed);
+    console.log(`WPM: ${wordsPerMinute}`);
+    console.log(`Accuracy: ${accuracy}%`);
+  }
+
+  startGame(
+    $inputField: JQuery<HTMLElement>,
+    $wordListContainer: JQuery<HTMLElement>
+  ) {
+    this.fetchWordList($wordListContainer);
+    $inputField.on('keydown', (event) => {
+      if (
+        this.currentWordIndex === 0 &&
+        !this.isTimerActive &&
+        /^[a-zA-Z]$/.test(event.key)
+      ) {
+        this.startTimer();
+      }
+      if (event.key === ' ') {
+        event.preventDefault();
+        if ($inputField.val() !== '') {
+          const typedWord = $inputField.val() || '';
+          switch (this.typingMode) {
+            case 'timer':
+              if (!this.isTimerActive) {
+                $inputField.prop('disabled', true);
+                this.displayResults();
+                return;
+              }
+              if (typedWord === this.generatedWords[this.currentWordIndex]) {
+                this.matchedWords.push(typedWord);
+              }
+              this.currentWordIndex++;
+              break;
+            case 'wordCount':
+              if (this.currentWordIndex < this.generatedWords.length - 1) {
+                if (typedWord === this.generatedWords[this.currentWordIndex]) {
+                  this.matchedWords.push(typedWord);
+                }
+                this.currentWordIndex++;
+              } else {
+                $inputField.prop('disabled', true);
+                this.displayResults();
+              }
+              break;
+          }
+        }
+        $inputField.val('');
+      }
+    });
+  }
+}
+
+export default TypeQuest;
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,110 +1,28 @@
-const $textField = $('.wordlist');
-const $inputField = $('#keyboard-input');
+import TypeQuest from './TypeQuest';
 
+const $typingInput = $('#keyboard-input');
+const $wordListContainer = $('.wordlist');
+
+const language = 'english';
+const includeNumbers = $('#numbers').is(':checked');
+const includePunctuation = $('#punctuation').is(':checked');
+const wordCount = $('input[name="word-count"]').val() as number;
+const timerDuration = $('input[name="timer-countdown"]').val() as number;
 const typingMode = $('input[name="typing-mode"]:checked').val() as
   | 'timer'
   | 'wordCount';
 
-let startTime: number;
-let wordList: string[];
-let currentWord: number;
-let isTimerActive: boolean;
-const correctWords: string[] = [];
+function init() {
+  const tq = new TypeQuest(
+    language,
+    wordCount,
+    timerDuration,
+    includeNumbers,
+    includePunctuation,
+    typingMode
+  );
 
-currentWord = 0;
-isTimerActive = false;
-
-function setText(_lang: string) {
-  wordList = [];
-  const wordCount = 25;
-  const lang = _lang.toLowerCase();
-  fetch('/random.json')
-    .then((response) => response.json())
-    .then((data) => {
-      const randomWords = data[lang];
-      while (wordList.length < wordCount) {
-        const randInx = Math.floor(Math.random() * randomWords.length);
-        const randomWord = randomWords[randInx] || '';
-        wordList.push(randomWord);
-        $textField.append(`<span>${randomWord} </span>`);
-      }
-    })
-    .catch((err) => console.error(err));
+  tq.startGame($typingInput, $wordListContainer);
 }
 
-function startTimer() {
-  switch (typingMode) {
-    case 'timer':
-      let seconds = 10;
-      const timer = window.setInterval(() => {
-        seconds--;
-        if (seconds === 0) {
-          window.clearInterval(timer);
-          isTimerActive = false;
-        }
-      }, 1000);
-      break;
-    case 'wordCount':
-      startTime = Date.now();
-      break;
-  }
-}
-
-function showResult() {
-  let words, minute, acc;
-  switch (typingMode) {
-    case 'timer':
-      words = correctWords.length;
-      minute = 10 / 60;
-      break;
-    case 'wordCount':
-      words = correctWords.length;
-      minute = (Date.now() - startTime) / 1000 / 60;
-      break;
-  }
-  acc = Math.floor((words / wordList.length) * 100);
-  let wpm = Math.floor(words / minute);
-  console.log(`WPM: ${wpm}`);
-  console.log(`Accuracy: ${acc}%`);
-}
-
-$inputField.on('keydown', (e) => {
-  if (currentWord === 0 && !isTimerActive && /^[a-zA-Z]$/.test(e.key)) {
-    startTimer();
-    isTimerActive = true;
-  }
-  if (e.key === ' ') {
-    e.preventDefault();
-    if ($inputField.val() !== '') {
-      switch (typingMode) {
-        case 'timer':
-          if (!isTimerActive) {
-            $inputField.prop('disabled', true);
-            showResult();
-            return;
-          }
-          const inputWord = $inputField.val();
-          if (inputWord && inputWord === wordList[currentWord]) {
-            correctWords.push(inputWord);
-          }
-          currentWord++;
-          break;
-        case 'wordCount':
-          if (currentWord < wordList.length - 1) {
-            const inputWord = $inputField.val();
-            if (inputWord && inputWord === wordList[currentWord]) {
-              correctWords.push(inputWord);
-            }
-            currentWord++;
-          } else {
-            $inputField.prop('disabled', true);
-            showResult();
-          }
-          break;
-      }
-    }
-    $inputField.val('');
-  }
-});
-
-setText('english');
+init();
